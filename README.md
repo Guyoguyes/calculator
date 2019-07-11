faeeb0dcb9cd3e59b516d7b2621b71ead797d7a4
Guyo <guyo@localhost.localdomain>
2019-07-11 11:33:28 +0300

    commit

---
+++ README.md
@@ -0,0 +1,28 @@
+ #  CALCULATOR
+
+ ## AUTHOR
+
+ ###[GUYO](https://github.com/Guyoguyes)
+
+ ## Description
+ ### this is a calculator created Html and Javascript
+
+ ##setup
+ 
+#### download a zip 
+#### extract the file
+#### open the Html file using your favourite browser
+
+ ##tech used
+
+###Html
+###Javascript
+
+
+####incase of any info leave me a message
+
+
+ ##license
+ ###MIT license
+ ###copyright@2019 Guyoguyes
+
---
+++ main.js
@@ -0,0 +1,40 @@
+var num1 = parseInt(prompt("Enter a number"));
+
+var num2 = parseInt(prompt("Enter another number"));
+
+var add = function(num1, num2){
+    return num1 + num2;
+    }
+    
+    alert(add(num1, num2));
+    
+var subtract = function(num1, num2){
+    return num1 - num2;
+    }
+    
+    alert(subtract(num1, num2));
+    
+ var multiply = function(num1, num2){
+    return num1 * num2;
+    }
+    
+    alert(multiply(num1, num2));
+    
+   var divide = function(num1, num2){
+    return num1 / num2;
+    }
+    
+    alert(divide(num1, num2));
+    
+    var reminder = function(num1, num2){
+    return num1 % num2;
+    }
+    
+    alert(reminder(num1, num2));
+    
+    var result = add,subtract,multiply,divide,reminder;
+
+    
+    
+    
+    
